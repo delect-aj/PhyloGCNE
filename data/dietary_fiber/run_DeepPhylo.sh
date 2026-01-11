8dfdd4eb8ab28e96aacf4e1b4a5a6bcc192373dd
delect-aj <1076169240@qq.com>
2026-01-11 19:28:34 +0800

    updata file

--- data/dietary_fiber/run_DeepPhylo.sh
+++ data/dietary_fiber/run_DeepPhylo.sh
@@ -18,5 +18,5 @@ python ${software_path}/deepphylo_classification.py \
     --test_table data/test_${i}.biom \
     --metadata_filename metadata.tsv \
     --pca_file_path PCA_32.txt --output_dir results --fold ${i} \
-    --epochs 100 -hs 80 -kec 13 -l 0.0001 -bs 64 -kep 4 -act relu --hidden_size 32
+    --epochs 50 -hs 80 -kec 13 -l 0.0001 -bs 64 -kep 4 -act relu --hidden_size 32
 done
